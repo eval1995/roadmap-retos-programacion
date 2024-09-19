95436f00aba1e0f21979a91cda445ececdc82ada
eval1995 <56900159+eval1995@users.noreply.github.com>
2024-09-19 16:27:36 -0500

    Create eval1995.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/eval1995.py
@@ -0,0 +1,29 @@
+# https://python.org
+# Primer comentario de una línea. 
+
+"""
+Primer comentario
+de multiples
+líneas
+"""
+
+'''
+Esta también es una manera de 
+hacer comentarios
+multiples en multiples líneas
+'''
+
+mi_variable = "Mi primera variable en Python"
+mi_variable = "Nuevo sentido de la variable"
+
+MI_CONSTANTE = "Esta será una constante" # Por convención
+
+mi_int = 117
+mi_float = 1.4
+mi_bool = True
+mi_bool = False
+mi_string = "Mi cadena de texto" 
+mi_other_string = 'Otra cadena de texto'
+
+print("¡Hola, Python!")
+
